cbaefda99e22d6fc3484e2948bb39506a37524c9
Nicholas Morrissey <n.mozza@hotmail.com>
2019-08-13 14:41:02 +0100

    Rails Task manager complete

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :complete)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
